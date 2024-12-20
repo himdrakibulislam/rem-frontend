f1186a89606dac08b22a985252d27e68e475711f
Rakibul Islam <himdrakibulislam@gmail.com>
2024-12-20 15:31:20 +0600

    day4560

--- src/components/Payments/BkashFrom.js
+++ src/components/Payments/BkashFrom.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
 
-import {
-  TextField,
-  Button,
-  Box,
-  Typography
-} from "@mui/material";
+import { TextField, Button, Box, Typography } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import { useAddonUpdateSettings } from "../../hooks/react-query/settings";
 import Image from "../Image";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const BkashForm = ({ defaultValues ,gateway}) => {
+const BkashForm = ({ defaultValues, gateway, isBank = false }) => {
   const {
     control,
     handleSubmit,
@@ -26,41 +22,76 @@ const BkashForm = ({ defaultValues ,gateway}) => {
   const updateAddonSettings = useAddonUpdateSettings();
 
   const onSubmit = (data) => {
-    const newData ={
-        settings : [
-            {
-                "key" : "logo_url",
-                "value" : data.logo_url
-            },
-            {
-                "key" : "theme_color",
-                "value" : data.theme_color
-            },
-            {
-                "key" : "receiver_number",
-                "value" : data.receiver_number
-            },
-            {
-                "key" : "payment_instructions",
-                "value" : data.payment_instructions
+    const newData = {
+      settings: [
+        {
+          key: "logo_url",
+          value: data.logo_url,
+        },
+        {
+          key: "theme_color",
+          value: data.theme_color,
+        },
+        isBank
+          ? {
+              key: "bank_name",
+              value: data.bank_name,
             }
-        ]
-    }
-    updateAddonSettings.mutate({data : newData,settingsType : gateway});
+          : {
+              key: "receiver_number",
+              value: data.receiver_number,
+            },
+        isBank && {
+          key: "account_name",
+          value: data.account_name,
+        },
+        isBank && {
+          key: "account_number",
+          value: data.account_number,
+        },
+        isBank && {
+          key: "branch_name",
+          value: data.branch_name,
+        },
+        isBank && {
+          key: "routing_numbe",
+          value: data.routing_numbe,
+        },
+        {
+          key: "payment_instructions",
+          value: data.payment_instructions,
+        },
+      ].filter(Boolean),
+    };
+    updateAddonSettings.mutate({ data: newData, settingsType: gateway });
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <Box textAlign="right">
+        <Button
+          sx={{
+            mt: 2,
+            backgroundImage: `linear-gradient(to right, ${gateway.theme_color}, ${gateway.theme_color})`, // Linear gradient
+            color: "white",
+            ":hover": {
+              color: "white",
+            },
+          }}
+        >
+          <DeleteIcon />
+        </Button>
+      </Box>
       <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "4px",
-                maxWidth: 150,
-                margin: "auto" 
-              }}
-            >
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "4px",
+          maxWidth: 150,
+          margin: "auto",
+        }}
+      >
         <Image src={defaultValues.logo_url} />
       </Box>
 
@@ -119,31 +150,120 @@ const BkashForm = ({ defaultValues ,gateway}) => {
           />
         )}
       />
-
-      {/* receiver number */}
-      <Controller
-        name="receiver_number"
-        control={control}
-        rules={{ required: "Receiver number is required" }}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            variant="outlined"
-            placeholder="Enter receiver number"
-            fullWidth
-            margin="normal"
-            error={!!errors.receiver_number}
-            helperText={errors.receiver_number?.message}
-          />
-        )}
-      />
+      {isBank ? (
+        <Controller
+          name="bank_name"
+          control={control}
+          rules={{ required: "Bank name is required" }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Enter receiver bank name"
+              fullWidth
+              margin="normal"
+              error={!!errors.bank_name}
+              helperText={errors.bank_name?.message}
+            />
+          )}
+        />
+      ) : (
+        <Controller
+          name="receiver_number"
+          control={control}
+          rules={{ required: "Receiver number is required" }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Enter receiver number"
+              fullWidth
+              margin="normal"
+              error={!!errors.receiver_number}
+              helperText={errors.receiver_number?.message}
+            />
+          )}
+        />
+      )}
+      {isBank && (
+        <Controller
+          name="account_name"
+          control={control}
+          rules={{ required: "Account name is required" }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Enter account name"
+              fullWidth
+              margin="normal"
+              error={!!errors.account_name}
+              helperText={errors.account_name?.message}
+            />
+          )}
+        />
+      )}
+      {isBank && (
+        <Controller
+          name="account_number"
+          control={control}
+          rules={{ required: "Account number is required" }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Enter account number"
+              fullWidth
+              margin="normal"
+              error={!!errors.account_number}
+              helperText={errors.account_number?.message}
+            />
+          )}
+        />
+      )}
+      {isBank && (
+        <Controller
+          name="branch_name"
+          control={control}
+          rules={{ required: "Branch name is required" }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Enter Branch name"
+              fullWidth
+              margin="normal"
+              error={!!errors.branch_name}
+              helperText={errors.branch_name?.message}
+            />
+          )}
+        />
+      )}
+      {isBank && (
+        <Controller
+          name="routing_numbe"
+          control={control}
+          rules={{ required: "Routing numbe is required" }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Enter routing numbe"
+              fullWidth
+              margin="normal"
+              error={!!errors.routing_numbe}
+              helperText={errors.routing_numbe?.message}
+            />
+          )}
+        />
+      )}
       {/*  payment instructions */}
       <Controller
         name="payment_instructions"
         control={control}
         rules={{ required: "Payment instructions is required" }}
         render={({ field }) => (
-            <>
+          <>
             <ReactQuill
               value={field.value || ""}
               onChange={(content) => field.onChange(content)}
@@ -172,15 +292,20 @@ const BkashForm = ({ defaultValues ,gateway}) => {
         )}
       />
 
-    
-
       {/* Submit Button */}
       <Button
         type="submit"
         fullWidth
         variant="contained"
         color="#CF2771"
-        sx={{ mt: 3 }}
+        sx={{
+          mt: 2,
+          backgroundImage: `linear-gradient(to right, ${gateway.theme_color}, ${gateway.theme_color})`, // Linear gradient
+          color: "white",
+          ":hover": {
+            color: "white",
+          },
+        }}
         startIcon={<SaveIcon />}
         disabled={updateAddonSettings.isLoading}
       >
--- src/components/Payments/PaymentsAddons.js
+++ src/components/Payments/PaymentsAddons.js
@@ -1,7 +1,8 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { useAddonSettings } from "../../hooks/react-query/settings";
 import ProgressBar from "../ProgressBar";
 import BkashForm from "./BkashFrom";
+import AddIcon from '@mui/icons-material/Add';
 
 function PaymentsAddons() {
   const { data, isLoading } = useAddonSettings();
@@ -10,55 +11,61 @@ function PaymentsAddons() {
   }
   return (
     <Grid container spacing={2}>
-      <Grid marginTop={2} item xs={12} sm={12} lg={12}>
+      <Grid
+        marginTop={2}
+        item
+        xs={12}
+        sm={12}
+        lg={12}
+        container
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Typography sx={{ mx: 2 }} variant="h6">
           Mobile Banking
         </Typography>
+        <Button> <AddIcon/> Add New</Button>
       </Grid>
-      <Grid marginTop={2} item xs={12} sm={12} lg={6}>
-        <Box
-          sx={{
-            margin: "auto",
-            padding: 3,
-            border: "1px solid #e0e0e0",
-            borderRadius: "8px",
-            boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <BkashForm defaultValues={data?.bkash} gateway="bkash" />
-        </Box>
-      </Grid>
-      <Grid marginTop={2} item xs={12} sm={12} lg={6}>
-        <Box
-          sx={{
-            margin: "auto",
-            padding: 3,
-            border: "1px solid #e0e0e0",
-            borderRadius: "8px",
-            boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <BkashForm defaultValues={data?.nagod} gateway="nagod" />
-        </Box>
-      </Grid>
+      {Object.entries(data)
+        .filter(([key, value]) => value.type === "mobile_bank")
+        .map(([key, value]) => (
+          <Grid marginTop={2} item xs={12} sm={12} lg={6}>
+            <Box
+              sx={{
+                margin: "auto",
+                padding: 3,
+                border: "1px solid #e0e0e0",
+                borderRadius: "8px",
+                boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <BkashForm defaultValues={value} gateway={value} />
+            </Box>
+          </Grid>
+        ))}
+
       <Grid marginTop={2} item xs={12} sm={12} lg={12}>
         <Typography sx={{ mx: 2 }} variant="h6">
           Bank Details
         </Typography>
       </Grid>
-      <Grid marginTop={2} item xs={12} sm={12} lg={6}>
-        <Box
-          sx={{
-            margin: "auto",
-            padding: 3,
-            border: "1px solid #e0e0e0",
-            borderRadius: "8px",
-            boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <BkashForm defaultValues={data?.duchbangla} gateway="duchbangla" />
-        </Box>
-      </Grid>
+      {Object.entries(data)
+        .filter(([key, value]) => value.type === "bank")
+        .map(([key, value]) => (
+          <Grid marginTop={2} item xs={12} sm={12} lg={6}>
+            <Box
+              sx={{
+                margin: "auto",
+                padding: 3,
+                border: "1px solid #e0e0e0",
+                borderRadius: "8px",
+                boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <BkashForm defaultValues={value} isBank gateway={value} />
+            </Box>
+          </Grid>
+        ))}
     </Grid>
   );
 }
